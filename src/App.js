84d496ab29057e87c7ca84d24323cd8befda338e
ArthurSlothouber <mailforarthur@yahoo.com>
2018-04-08 14:54:25 +0200

    CSS changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">This is the Hangman Game</h1>
+          <h1 className="App-title">Play The Hangman Game</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+
+
         </p>
       </div>
     );
